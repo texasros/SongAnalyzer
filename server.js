52ad0b85d37c1f6617e05f2acd1dd2d5f4a891d6
1i3R <157350265+texasros@users.noreply.github.com>
2024-07-27 16:55:36 -0500

    port change

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.phase(locomotive.boot.views());
 // start an HTTP server.  Additional phases can be inserted as needed, which
 // is particularly useful if your application handles upgrades from HTTP to
 // other protocols such as WebSocket.
-app.phase(require('bootable-environment')(__dirname + '/config/environments'));
-app.phase(bootable.initializers(__dirname + '/config/initializers'));
-app.phase(locomotive.boot.routes(__dirname + '/config/routes'));
-app.phase(locomotive.boot.httpServer(process.env.PORT || 80, '0.0.0.0'));
+const path = require('path');
+
+app.phase(require('bootable-environment')(path.join(__dirname, 'config', 'environments')));
+app.phase(bootable.initializers(path.join(__dirname, 'config', 'initializers')));
+app.phase(locomotive.boot.routes(path.join(__dirname, 'config', 'routes')));
+app.phase(locomotive.boot.httpServer(parseInt(process.env.PORT) || 3000, '127.0.0.1'));
 
 // Boot the application.  The phases registered above will be executed
 // sequentially, resulting in a fully initialized server that is listening
